cd829971036a1e465c054b125c7fc97ba432fe41
NaTsUDraG29 <106482427+NaTsUDraG29@users.noreply.github.com>
2025-10-01 18:33:46 +0530

    Update TPS5288controller.c

--- TPS5288controller.c
+++ TPS5288controller.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
-#include "tps55288.h"   // your driver header
+#include "tps55288.h"   
 #include <string.h>
 #include <math.h>
 #include "hardware/watchdog.h"
@@ -11,7 +11,7 @@
 
 
 // I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
+
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define I2C_PORT i2c0
 #define I2C_SDA 8
@@ -175,3 +175,4 @@ int main()
         }
     }
 }
+
